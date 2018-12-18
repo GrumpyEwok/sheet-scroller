7db8cc897f59c12085f854e8ed2f177d535b17b6
GrumpyEwok <evanfilkins@gmail.com>
2018-12-17 16:39:11 -0800

    Adjust styling.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Footer from './components/Footer';
 import Landing from './components/Landing';
 import Profile from './components/Profile';
 
@@ -15,6 +16,7 @@ class App extends Component {
             <Switch>
               <Route path='/user' render={()=> <Profile /* Props go here. */ />} />
             </Switch>
+            <Footer/>
         </div>
       </Router>
     );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer(){
+
+  return(
+    <div className='Footer'>
+      <em>I'm a link!</em>
+      <em>I'm a link!</em>
+      <em>I'm a link!</em>
+    </div>
+  )
+}
+
+export default Footer;
